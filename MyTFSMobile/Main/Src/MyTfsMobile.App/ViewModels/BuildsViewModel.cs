b7ae624251ba218077441bb3c79b82d134dda080
Tommy Indregaard <CRAYON\tomindre>
2013-08-21 22:27:20 +0200

    tap hold for å få contextmeny på build liste.

--- MyTFSMobile/Main/Src/MyTfsMobile.App/ViewModels/BuildsViewModel.cs
+++ MyTFSMobile/Main/Src/MyTfsMobile.App/ViewModels/BuildsViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using MyTfsMobile.App.enums;
 
 namespace MyTfsMobile.App.ViewModels
@@ -20,11 +22,13 @@ namespace MyTfsMobile.App.ViewModels
 
         // my/favourite/all team buils
         private string buildSection = "Builds";
+
         public string BuildSection
         {
             get { return buildSection; }
-            set{ 
-                if(buildSection == value) return;
+            set
+            {
+                if (buildSection == value) return;
                 buildSection = value;
                 NotifyPropertyChanged("BuildSection");
             }
@@ -34,10 +38,53 @@ namespace MyTfsMobile.App.ViewModels
 
         public void LoadData()
         {
-            BuildItems.Add(new BuildViewModel { BuildId = 1001, BuildName = "MyTfsMobile.ci", BuildDate = new DateTime(2013, 7, 5, 22, 22, 45), BuildStatus = BuildStatus.Failed, BuildMessages = @"No such folder c:\Nofolder"});
-            BuildItems.Add(new BuildViewModel { BuildId = 1002, BuildName = "MyTfsMobile.ci", BuildDate = new DateTime(2013, 7, 5, 22, 54, 12), BuildStatus = BuildStatus.Partial, BuildMessages = @"0/2 unit tests passed...blablabla" });
-            BuildItems.Add(new BuildViewModel { BuildId = 1003, BuildName = "MyTfsMobile.ci", BuildDate = new DateTime(2013, 7, 5, 23, 02, 33), BuildStatus = BuildStatus.Ok, BuildMessages = @"" });
+            BuildItems.Add(new BuildViewModel
+                           {
+                               BuildId = 1001,
+                               BuildName = "MyTfsMobile.ci",
+                               BuildDate = new DateTime(2013, 7, 5, 22, 22, 45),
+                               BuildStatus = BuildStatus.Failed,
+                               BuildMessages = @"No such folder c:\Nofolder"
+                           });
+            BuildItems.Add(new BuildViewModel
+                           {
+                               BuildId = 1002,
+                               BuildName = "Master.ci",
+                               BuildDate = new DateTime(2013, 7, 5, 22, 54, 12),
+                               BuildStatus = BuildStatus.Partial,
+                               BuildMessages = @"0/2 unit tests passed...blablabla"
+                           });
+            BuildItems.Add(new BuildViewModel
+                           {
+                               BuildId = 1003,
+                               BuildName = "TFS.ci",
+                               BuildDate = new DateTime(2013, 7, 5, 23, 02, 33),
+                               BuildStatus = BuildStatus.Ok,
+                               BuildMessages = @""
+                           });
             IsDataLoaded = true;
+        }
+
+
+        private RelayCommand<BuildViewModel> queueBuildCommand;
+        public RelayCommand<BuildViewModel> QueueBuildCommand
+        {
+            get
+            {
+                return queueBuildCommand ?? (queueBuildCommand = new RelayCommand<BuildViewModel>(
+                    async (build) =>
+                    {
+                        await QueueBuild(build);
+                    }));
+            }
+        }
+
+
+        static async private Task<bool> QueueBuild(BuildViewModel build)
+        {
+            // do work with build
+            
+            return true;
         }
     }
 }
